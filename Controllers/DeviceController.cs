f6ab7f8da83fabd9f9e02e067effa2456fa8a62f
ben <9029708+nullcoalescence@users.noreply.github.com>
2023-11-22 17:59:28 -0600

    created device controller, service, and index view

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wakeonlan_server.Models;
+using wakeonlan_server.Services;
+
+namespace wakeonlan_server.Controllers
+{
+    public class DeviceController : Controller
+    {
+        private readonly ILogger<DeviceController> logger;
+        private readonly DeviceService deviceService;
+
+        public DeviceController(ILogger<DeviceController> logger, DeviceService deviceService)
+        {
+            this.logger = logger;
+            this.deviceService = deviceService;
+        }
+
+        public IActionResult Index()
+        {
+            var devices = this.deviceService.GetAllDevices();
+
+            //ViewData["devices"] = devices;
+            return View(devices);
+        }
+    }
+}
