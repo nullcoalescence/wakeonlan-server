ca2bf2150abfd02b118143b819d3c97ba082ee58
ben <9029708+nullcoalescence@users.noreply.github.com>
2023-11-22 16:15:06 -0600

    configured entity framework for sqlite provider

---
+++ Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace wakeonlan_server.Models
+{
+    public class Device
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MacAddress { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using wakeonlan_server.Db;
+
 namespace wakeonlan_server
 {
     public class Program
@@ -8,6 +10,8 @@ namespace wakeonlan_server
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<WakeOnLanContext>();
 
             var app = builder.Build();
 
