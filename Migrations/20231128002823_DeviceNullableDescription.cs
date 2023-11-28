d90c772af275a4923b76cfa48dcf2a0d07f5dfe2
ben <9029708+nullcoalescence@users.noreply.github.com>
2023-11-27 18:29:35 -0600

    add DataAnnotations to Device model and error messages to model. generate new db migrations for nullable Device Description field

---
+++ Migrations/20231128002823_DeviceNullableDescription.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wakeonlan_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeviceNullableDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Devices",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Devices",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
--- Models/Device.cs
+++ Models/Device.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
 namespace wakeonlan_server.Models
 {
@@ -8,13 +7,16 @@ namespace wakeonlan_server.Models
         [Required()]
         public int Id { get; set; }
 
-        [MaxLength(50)] 
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50, ErrorMessage = "Name must be 50 characters or less")] 
         public string Name { get; set; }
 
-        [MaxLength(255)]
-        public string Description { get; set; }
+        [MaxLength(255, ErrorMessage = "Description must be 255 characters or less")]
+        public string? Description { get; set; }
 
-        //[RegularExpression("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})$")]
+        // https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
+        [Required(ErrorMessage = "Mac address is required")]
+        [RegularExpression("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", ErrorMessage = "Mac address must be in the format 'A1:B2:C3:D4:E5:F6'")]
         [Display(Name="Mac Address")]
         public string MacAddress { get; set; }
     }
